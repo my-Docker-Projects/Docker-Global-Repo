56e1b885a90a2bad96c00c9db446e8fc1185ccb5
Aleksandruk2 <aaleksandruk2018@gmail.com>
2026-01-29 15:42:55 +0200

    Init commit

---
+++ Classwork/Projects/OneMVCProject/OneMVCProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OneMVCProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
